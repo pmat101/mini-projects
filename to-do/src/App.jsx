3d0282422799f6426c7889c146efa2bded74355c
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-04-14 16:35:41 +0530

    moved form data to its own component

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -9,18 +9,6 @@ export default function App() {
   const [initialState, setNewState] = useState("placeholder text");
   return (
     <>
-      <form onSubmit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item"></label>
-          <input
-            value={initialState}
-            onInput={(e) => setNewState(e.target.value)}
-            type="text"
-            id="item"
-          />
-        </div>
-        <button className="btn">Add</button>
-      </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
         <li>
--- to-do/src/newToDoForm.jsx
+++ to-do/src/newToDoForm.jsx
@@ -1,3 +1,16 @@
 export default function NewToDoForm() {
-  return <></>;
+  return (
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item"></label>
+        <input
+          value={initialState}
+          onInput={(e) => setNewState(e.target.value)}
+          type="text"
+          id="item"
+        />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+  );
 }
