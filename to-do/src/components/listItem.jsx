feca990decbf826fc0b01896dd4f275cfda11b54
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-04-20 18:04:17 +0530

    :p

--- to-do/src/components/listItem.jsx
+++ to-do/src/components/listItem.jsx
@@ -1,11 +1,20 @@
 export default function ToDoItem() {
   return (
-    <li>
-      <label>
-        <input type="checkbox" />
-        Item 1
-      </label>
-      <button className="btn btn-danger">Delete</button>
-    </li>
+    <>
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 2
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    </>
   );
 }
