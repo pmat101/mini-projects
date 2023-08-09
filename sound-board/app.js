b479b1d06e8d8ca2b78ecc0d91169dd6f90cfd53
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-08-09 14:35:44 +0530

    play/pause working fine now

--- sound-board/app.js
+++ sound-board/app.js
@@ -1,28 +1,7 @@
 let mySound;
+
 let play = (x) => {
-  switch (x) {
-    case "Applause":
-      mySound = new Audio("audio/applause.mp3");
-      mySound.play();
-      break;
-    case "Boo":
-      mySound = new Audio("audio/boo.mp3");
-      mySound.play();
-      break;
-    case "Gasp":
-      mySound = new Audio("audio/gasp.mp3");
-      mySound.play();
-      break;
-    case "Tada":
-      mySound = new Audio("audio/tada.mp3");
-      mySound.play();
-      break;
-    case "Victory":
-      mySound = new Audio("audio/victory.mp3");
-      mySound.play();
-      break;
-    case "Wrong":
-      mySound = new Audio("audio/wrong.mp3");
-      mySound.play();
-  }
+  if (mySound != undefined) mySound.pause();
+  mySound = new Audio(`audio/${x}.mp3`);
+  mySound.play();
 };
