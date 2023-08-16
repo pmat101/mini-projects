17a0c2c9d2ab0b2504e52c5778854582bf52e958
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-08-16 20:56:23 +0530

    JS part remains

---
+++ sticky-navigation/app.js
@@ -0,0 +1,3 @@
+let navbar = document.querySelector("nav");
+// if (navbar.style.position == "fixed") navbar.style.backgroundColor = "red";
+// if (navbar.style.position == "absolute") navbar.style.backgroundColor = "green";
