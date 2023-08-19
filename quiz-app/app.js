79e9251ef13be54b2ed484e3a8e980801164566b
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-08-19 22:28:51 +0530

    Major work done minor functionality remain

--- quiz-app/app.js
+++ quiz-app/app.js
@@ -5,7 +5,7 @@ const questions = [
     op2: "C",
     op3: "Python",
     op4: "JavaScript",
-    a: "JavaScript",
+    a: 4,
   },
   {
     q: "What does CSS stand for?",
@@ -13,7 +13,7 @@ const questions = [
     op2: "Cascading Style Sheets",
     op3: "Cascading Simple Sheets",
     op4: "Cars SUVs Sailboats",
-    a: "Cascading Style Sheets",
+    a: 2,
   },
   {
     q: "What does HTML stand for?",
@@ -21,7 +21,7 @@ const questions = [
     op2: "Hypertext Markdown Language",
     op3: "Hyperloop Machine Language",
     op4: "Helicopters Terminals Motorboats Lamborginis",
-    a: "Hypertext Markup Language",
+    a: 1,
   },
   {
     q: "What year was JavaScript launched?",
@@ -29,27 +29,50 @@ const questions = [
     op2: "1995",
     op3: "1994",
     op4: "none of the above",
-    a: "1995",
+    a: 2,
   },
 ];
 
 let score = 0;
 let count = 0;
-let q = document.getElementById("questions");
+let answer = 0;
+
+let que = document.getElementById("questions");
 let op1 = document.querySelector("label[for='1']");
 let op2 = document.querySelector("label[for='2']");
 let op3 = document.querySelector("label[for='3']");
 let op4 = document.querySelector("label[for='4']");
 let submit = document.querySelector("input[type='submit']");
 
-let display = () => {
-  q.innerText = questions[count].q;
-  op1.innerText = questions[count].op1;
-  op2.innerText = questions[count].op2;
-  op3.innerText = questions[count].op3;
-  op4.innerText = questions[count].op4;
-  count++;
-  display();
+que.innerText = questions[count].q;
+op1.innerText = questions[count].op1;
+op2.innerText = questions[count].op2;
+op3.innerText = questions[count].op3;
+op4.innerText = questions[count].op4;
+
+let selected = (x) => {
+  answer = x;
 };
 
-display();
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  count++;
+  if (count < 4) {
+    que.innerText = questions[count].q;
+    op1.innerText = questions[count].op1;
+    op2.innerText = questions[count].op2;
+    op3.innerText = questions[count].op3;
+    op4.innerText = questions[count].op4;
+    submit.addEventListener("click", function () {
+      if (answer == questions[count].a) {
+        score++;
+        answer = 0;
+      }
+      console.log(count);
+    });
+  } else {
+    // let para = document.createElement("p");
+    // para.innerText = score;
+    document.body.innerHTML = score;
+  }
+});
