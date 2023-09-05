0ee9968d2eb6768d093d38274a6debf3cb247caf
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-09-05 13:20:32 +0530

    Random Choice Picker project overall working properly, finishing touches remain..

--- random-choice-picker/app.js
+++ random-choice-picker/app.js
@@ -3,8 +3,8 @@ let count = 0;
 
 let enterTheDragon = (last, string) => {
   if (string.length == 0 || last.key == ",") createButton();
-  else if (last.key != ",") addText(string);
-  else if (last.key == "Enter") console.log("ok");
+  else if (last.key != "," && last.key != "Enter") addText(string);
+  else choice(string);
 };
 
 let createButton = () => {
@@ -17,5 +17,12 @@ let createButton = () => {
 let addText = (val) => {
   let arr = val.split(",");
   let oldBtn = document.getElementById(count);
-  oldBtn.innerText = arr[length];
+  oldBtn.innerText = arr[arr.length - 1];
+};
+
+let choice = (x) => {
+  let arr = x.split(",");
+  let rand = Math.ceil(Math.random() * arr.length);
+  let chosen = document.getElementById(rand);
+  chosen.style.backgroundColor = "red";
 };
